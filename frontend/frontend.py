8f1e1c94b493bdb6792c30c936c251628bd90248
Khadeeja Ahmed <@users.noreply.github.com>
2025-11-15 10:05:54 +0000

    did some more moving around of files/folder

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+
 
 st.set_page_config(page_title='Dummy book Recommender', page_icon=':books:')
 st.title('Your Dummy Book Recommender')
@@ -8,9 +10,11 @@ book_image = st.file_uploader('Upload a picture of available books:',
 goodreads_url = st.text_input('Paste your Goodreads profile url here')
 
 if st.button('Generate recommendation'):
-    if not book_image:
-        st.warning("Please upload a picture of the shelf you're browing")
-    if not goodreads_url:
-        st.warning('Please enter a Goodreads profile url')
-    else:
-        st.write('Your book recommendation:', 'The Lord of the Rings')
+    res = requests.get(url='https://the-book-thrift-43012920273.europe-west1.run.app/predict').json()
+    st.write(res)
+    # if not book_image:
+    #     st.warning("Please upload a picture of the shelf you're browing")
+    # if not goodreads_url:
+    #     st.warning('Please enter a Goodreads profile url')
+    # else:
+    #     st.write('Your book recommendation:', 'The Lord of the Rings')
