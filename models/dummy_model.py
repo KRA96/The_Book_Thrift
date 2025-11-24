f8499f60fcac5ceee47c6ec6f9828463666f42d8
Khadeeja Ahmed <@users.noreply.github.com>
2025-11-24 22:10:00 +0000

    added collaborative filter .py file

--- models/dummy_model.py
+++ models/dummy_model.py
@@ -3,5 +3,12 @@ def dummy_rec():
     book = get_random_book()
     return(f"A book in your list is {book}")
 
+
+def generate_rec(user_data):
+    """ 
+    This function will take a user's data and generate a recommendation
+    based currently on the collaborative filter system.
+    """
+
 if __name__ == '__main__':
     print(dummy_rec())
