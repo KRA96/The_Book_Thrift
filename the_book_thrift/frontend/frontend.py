15b48e315c0af1694ad73b395335e5a31ac08f14
Khadeeja Ahmed <@users.noreply.github.com>
2025-11-26 21:28:12 +0000

    changed streamlit code to match new model

--- the_book_thrift/frontend/frontend.py
+++ the_book_thrift/frontend/frontend.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import requests
-
+from the_book_thrift.ML_logic.recommender import ALSRecommender
 
 st.set_page_config(page_title='Dummy book Recommender', page_icon=':books:')
-st.title('Your Dummy Book Recommender')
-book_image = st.file_uploader('Upload a picture of available books:',
-                 type=['png', 'jpeg', 'jpg'])
+st.title('MVP Book Recommender')
 
-goodreads_url = st.text_input('Paste your Goodreads profile url here')
+@st.cache_resource
+def load_recommender():
+    return ALSRecommender()
 
-if st.button('Generate recommendation'):
+recsys = load_recommender()
+
+uploaded_file = st.file_uploader("Upload your Goodreads CSV", type=["csv"])
+
+# goodreads_url = st.text_input('Paste your Goodreads profile url here')
+
+if uploaded_file is not None and st.button("Get recommendations"):
     res = requests.get(url='https://the-book-thrift-43012920273.europe-west1.run.app/predict').json()
     st.write(res)
+
+
     # if not book_image:
     #     st.warning("Please upload a picture of the shelf you're browing")
     # if not goodreads_url:
